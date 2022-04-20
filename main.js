5881002e308d1b7e50679fadaf1ecedd0df56a93
[200~Kamil-Kiyamov~ <kamil.prog@yandex.ru>
2022-04-20 16:26:58 +0300

    работает свайпер

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-var swiper = new Swiper(".mySwiper", {
-    pagination: {
-        el: ".swiper-pagination",
-    },
-});
+// var swiper = new Swiper(".mySwiper", {
+//     pagination: {
+//         el: ".swiper-pagination",
+//     },
+// });
 //
 // function() {
 //     'use strict';
@@ -52,25 +52,40 @@ var swiper = new Swiper(".mySwiper", {
 // if (window.matchMedia("(min-width: 768px)").matches) {
 //     mySwiper.destroy(true, true);
 //
-// } else {
-//     enableSwiper();
-// }
+// // } else {
+// //     enableSwiper();
+// // }
+//
+// const breakpoint = window.matchMedia( '(max-width:768)' );
+//
+// const breakpointChecker = function() {
+//     // if larger viewport and multi-row layout needed
+//     if ( breakpoint.matches === true ) {
+//         // clean up old instances and inline styles when available
+//         if ( swiper !== undefined ) mySwiper.destroy( true, true );
+//         // or/and do nothing
+//         return;
+//         // else if a small viewport and single column layout needed
+//     } else if ( breakpoint.matches === false ) {
+//         // fire small viewport version of swiper
+//         return enableSwiper();
+//     }
+// };
 
-const breakpoint = window.matchMedia( '(max-width:768)' );
 
-const breakpointChecker = function() {
-    // if larger viewport and multi-row layout needed
-    if ( breakpoint.matches === true ) {
-        // clean up old instances and inline styles when available
-        if ( swiper !== undefined ) mySwiper.destroy( true, true );
-        // or/and do nothing
-        return;
-        // else if a small viewport and single column layout needed
-    } else if ( breakpoint.matches === false ) {
-        // fire small viewport version of swiper
-        return enableSwiper();
-    }
-};
+// window.screen.width
+
+if (document.documentElement.scrollWidth < 768) {
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: "auto",
+        spaceBetween: 16,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+    });
+}
+
 
 
 
