5038cba6a7d0327190538664eb0d227424c48140
[200~Kamil-Kiyamov~ <kamil.prog@yandex.ru>
2022-04-26 16:26:02 +0300

    пропадает svg при нажатии на кнопку.

--- main.js
+++ main.js
@@ -10,16 +10,32 @@ if (document.documentElement.scrollWidth < 768) {
     });
 }
 
-let elem = document.querySelectorAll('.hidden-elem');
+let elem = document.querySelector('.hidden-elem')
+let elems = document.querySelectorAll('.hidden-elem');
 let hidingButton = document.querySelector('.brands__hiding-button');
+let hidingButtonSpan = document.querySelector('brands__hiding-button-span')
+console.log(elem.classList.contains("hidden-elem"))
 
-for (let el of elem) {
-    hidingButton.onclick = function () {
+hidingButton.onclick = function () {
+    for (let el of elems) {
         el.classList.toggle('hidden-elem');
     }
+    // if (elem.classList.contains("hidden-elem") != true) {
+    //     hidingButtonSpan.innerText = "Скрыть"
+    // } else {hidingButtonSpan.innerText = "Показать все"}
+
+    if (elem.classList.contains("hidden-elem") != true) {
+        hidingButton.textContent = "Скрыть"
+    } else {hidingButton.textContent = "Показать все"}
 }
 
-// if (hidingButton.onclick === true) {
-//     elem.classList.toggle('hidden-elem')
-// }
+// if (clicks % 2 === 0) {
+//     hidingButton.textContent = "Показать всё";
+// } else (hidingButton.textContent = "Скрыть")
+//
+
+//
+// if (elem.classList.contains('hidden-elem')){
+// } else {hidingButton.textContent = "Скрыть всё"}
+
 
