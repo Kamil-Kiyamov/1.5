acefb6f7992dff684fc5da0ce6d8965667585c29
[200~Kamil-Kiyamov~ <kamil.prog@yandex.ru>
2022-04-27 18:00:38 +0300

    svg не пропадаетпри нажатии на кнопку. Надо добавить стрелки для кнопки

--- main.js
+++ main.js
@@ -17,35 +17,35 @@ let hidingButtonSpan = document.querySelector('brands__hiding-button-span')
 console.log(elem.classList.contains("hidden-elem"))
 let view = document.querySelector('.brands__hiding-button-view')
 let hide = document.querySelector('.brands__hiding-button-hide')
+// let i = 1;
 
+let svg = document.querySelector('.hiding-button__svg')
 hidingButton.onclick = function () {
     for (let el of elems) {
         el.classList.toggle('hidden-elem');
     }
-    // if (elem.classList.contains("hidden-elem") != true) {
-    //     hidingButtonSpan.innerText = "Скрыть"
-    // } else {hidingButtonSpan.innerText = "Показать все"}
+    let i=1;
+    let b = i+1;
+    console.log(b)
 
-    // if (elem.classList.contains("hidden-elem") != true) {
-    //     hidingButton.textContent = "Скрыть"
-    // } else {hidingButton.textContent = "Показать все"}
-
+    // svg.style.transform = 'rotate(180deg)'
     if (elem.classList.contains("hidden-elem") != true) {
-        view.style.display = 'flex';
-        hide.style.display = 'none';
-    } else {
         view.style.display = 'none';
         hide.style.display = 'flex';
+        svg.style.transform = 'rotate(180deg)'
+    } else {
+        view.style.display = 'flex';
+        hide.style.display = 'none';
+        svg.style.transform = 'rotate(0deg)'
     }
+
+
 }
+// if (elem.classList.contains("hidden-elem") != true) {
+//     hidingButtonSpan.innerText = "Скрыть"
+// } else {hidingButtonSpan.innerText = "Показать все"}
 
-// if (clicks % 2 === 0) {
-//     hidingButton.textContent = "Показать всё";
-// } else (hidingButton.textContent = "Скрыть")
-//
-
-//
-// if (elem.classList.contains('hidden-elem')){
-// } else {hidingButton.textContent = "Скрыть всё"}
-
+// if (elem.classList.contains("hidden-elem") != true) {
+//     hidingButton.textContent = "Скрыть"
+// } else {hidingButton.textContent = "Показать все"}
 
